affc4fbb60bf48de7076eadb8bfe642356508207
James <jramire678@gmail.com>
2020-04-21 18:20:52 -0400

    Updates

---
+++ precache-manifest.2d8aad099d295ecca7710c6c067fa71a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4b86b0cdafccdd3ed7962aba427d0f",
+    "url": "/jramire16.github.io/index.html"
+  },
+  {
+    "revision": "f284da588df38b7bb934",
+    "url": "/jramire16.github.io/static/css/main.ad9142c1.chunk.css"
+  },
+  {
+    "revision": "05d7e821d2239a621932",
+    "url": "/jramire16.github.io/static/js/2.0c41ae97.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/jramire16.github.io/static/js/2.0c41ae97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f284da588df38b7bb934",
+    "url": "/jramire16.github.io/static/js/main.5b8033aa.chunk.js"
+  },
+  {
+    "revision": "c5ceed5e54b6841e68b2",
+    "url": "/jramire16.github.io/static/js/runtime-main.22ab6128.js"
+  },
+  {
+    "revision": "35bf67485477067466fc1bd6bb4f72ce",
+    "url": "/jramire16.github.io/static/media/Art1.35bf6748.png"
+  },
+  {
+    "revision": "fdeb1f9c9e8b85587fdc3fc28c3ccc6b",
+    "url": "/jramire16.github.io/static/media/Art2.fdeb1f9c.png"
+  },
+  {
+    "revision": "688946fdb1ab6141ea8affd0f0a59891",
+    "url": "/jramire16.github.io/static/media/Art3.688946fd.png"
+  },
+  {
+    "revision": "330713139b1a9ec3c3cebbc57ced8a9d",
+    "url": "/jramire16.github.io/static/media/Profile.33071313.png"
+  },
+  {
+    "revision": "369a94198c4ae7820513f19babb30d86",
+    "url": "/jramire16.github.io/static/media/Study.369a9419.png"
+  },
+  {
+    "revision": "8651ea46398c5a25c88aebaf7c3c9319",
+    "url": "/jramire16.github.io/static/media/Study2.8651ea46.png"
+  },
+  {
+    "revision": "be9d84534f4f3f0f719694739dfe441d",
+    "url": "/jramire16.github.io/static/media/Study3.be9d8453.png"
+  },
+  {
+    "revision": "03be077da413a7a3f7c658a45c34a5e0",
+    "url": "/jramire16.github.io/static/media/Vid1.03be077d.mp4"
+  },
+  {
+    "revision": "bea3feed34d4a9dd166ab375bb33865d",
+    "url": "/jramire16.github.io/static/media/Vid2.bea3feed.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.13fb7ec76f02327142e045a8450a9344.js"
+  "/jramire16.github.io/precache-manifest.2d8aad099d295ecca7710c6c067fa71a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jramire16.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
